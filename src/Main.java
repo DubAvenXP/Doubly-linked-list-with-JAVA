feb1fee37d0d84ebe9a13e6b089a161555dbfce9
Alejandro Dubon <alejandrodubon88@gmail.com>
2021-08-08 14:15:19 -0600

    [DONE] Doubly linked list

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        ListaDoble listaDoble = new ListaDoble();
+        listaDoble.append(0);
+        listaDoble.append(1);
+        listaDoble.append(2);
+        listaDoble.append(3);
+        listaDoble.append(4);
+        listaDoble.append(5);
+        listaDoble.append(6);
+        listaDoble.append(7);
+
+        listaDoble.print();
+    }
+}
